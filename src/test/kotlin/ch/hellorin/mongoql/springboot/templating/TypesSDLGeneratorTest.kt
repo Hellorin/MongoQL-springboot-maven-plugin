58fb984a2bd04edb061480ee193be7c29f7b73ce
David Lawrence <davi.lawrence@gmail.com>
2020-02-18 06:20:31 +0100

    Fixed test

--- src/test/kotlin/ch/hellorin/mongoql/springboot/templating/TypesSDLGeneratorTest.kt
+++ src/test/kotlin/ch/hellorin/mongoql/springboot/templating/TypesSDLGeneratorTest.kt
@@ -2,28 +2,50 @@ package ch.hellorin.mongoql.springboot.templating
 
 import com.hellorin.mongoql.Attribute
 import com.hellorin.mongoql.Type
-import org.junit.Assert.*
+import org.junit.Assert
 import org.junit.Test
 import java.io.File
+import java.util.stream.Collectors
 
-internal class TypesSDLGeneratorTest() {
+internal class TypesSDLGeneratorTest {
     @Test
-    fun `test`() {
+    fun `generate sdl types file`() {
         // Given
-        val attributes1 = listOf<Attribute>(
+        val filePath = listOf(".", "target", "tests", "testFolder", "graphql").joinToString(separator = File.separator)
+        val attributes1 = listOf(
                 Attribute("name", setOf("String"), false),
-                Attribute("age", setOf("Int"), true)
+                Attribute("age", setOf("Int"), true),
+                Attribute("bankAccount", setOf("Money"), true)
         )
         val type1 = Type("Person", attributes1)
 
+        val attributes2 = listOf(
+                Attribute("debit", setOf("Int"), false),
+                Attribute("credit", setOf("Int"), false)
+        )
+        val type2 = Type("Money", attributes2)
+
         val types = listOf(
-                type1
+                type1,
+                type2
         )
 
         // When
-        TypesSDLGenerator().generate(types)
+        TypesSDLGenerator(folder = filePath).generate(types)
 
         // Then
-        File("./generated-ressources/graphql/")
+        val file = File(listOf(filePath, "types.graphqls").joinToString(File.separator))
+        Assert.assertTrue(file.exists())
+
+        val expectedFileContent = File(listOf(".", "src", "test", "resources", "test-resources", "types.graphqls").joinToString(File.separator)).bufferedReader().lines().collect(Collectors.joining("\n"))
+        val fileContent = file.bufferedReader().lines().collect(Collectors.joining("\n"))
+        Assert.assertEquals(cleanStringForAssertion(expectedFileContent), cleanStringForAssertion(fileContent))
     }
+
+    private fun cleanStringForAssertion(input : String) : String = input
+                .replace("\n", "")
+                .replace("\r", "")
+                .replace("\t", "")
+                .replace(" ", "")
+                .trim()
 }
